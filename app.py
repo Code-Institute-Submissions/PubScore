0b4a1671f50a2e436cc58f6ab92189c7c627c7d5
FrancesVugts <francesvugts@hotmail.com>
2020-08-06 15:11:36 +0000

    install flask-pymongo and add to requirements.txt

--- app.py
+++ app.py
@@ -1,33 +1,4 @@
 import os
-import pymongo
-if os.path.exists("env.py"):
-    import env
-
-
-MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "PubScore"
-COLLECTION = "competitors"
-
-
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("Mongo is connected")
-        return conn
-    except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
-
-
-conn = mongo_connect(MONGO_URI)
-
-coll = conn[DATABASE][COLLECTION]
-
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
-
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -62,3 +33,37 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
+
+
+
+
+
+
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "PubScore"
+COLLECTION = "competitors"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
