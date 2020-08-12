01a90c37415946595af7f62cd836b5219ecf00eb
FrancesVugts <francesvugts@hotmail.com>
2020-08-12 13:13:44 +0000

    Reorganise pages

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 @app.route("/overview")
