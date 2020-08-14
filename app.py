79072e6463fd7ebcfc31c9f8285239ca2000f1a3
FrancesVugts <francesvugts@hotmail.com>
2020-08-14 09:24:28 +0000

    Finish setting up the login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, g, redirect, request, url_for
 from flask_oidc import OpenIDConnect
+from okta import UsersClient
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import datetime
@@ -15,11 +16,22 @@ app.config["OIDC_COOKIE_SECURE"] = False
 app.config["OIDC_CALLBACK_ROUTE"] = "/oidc/callback"
 app.config["OIDC_SCOPES"] = ["openid", "email", "profile"]
 app.config["SECRET_KEY"] = SECRET_KEY
+app.config["OIDC_ID_TOKEN_COOKIE_NAME"] = "oidc_token"
 oidc = OpenIDConnect(app)
+okta_client = UsersClient("https://dev-395248.okta.com",
+                          "00SvA-wtwtZrunBeOfMPCmIdo788BQcjWq_81b3dwK")
 MONGO_URI = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = 'PubScore'
 app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
+
+
+@app.before_request
+def before_request():
+    if oidc.user_loggedin:
+        g.user = okta_client.get_user(oidc.user_getfield("sub"))
+    else:
+        g.user = None
 
 
 @app.route("/")
@@ -29,11 +41,13 @@ def index():
 
 
 @app.route("/login")
+@oidc.require_login
 def login():
-    return render_template("login.html")
+    return redirect(url_for(".dashboard"))
 
 
 @app.route("/dashboard")
+@oidc.require_login
 def dashboard():
     return render_template("dashboard.html")
 
