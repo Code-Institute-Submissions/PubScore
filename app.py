f1ceb2396dd9ae29c44435f1136a877b2f5c3056
FrancesVugts <francesvugts@hotmail.com>
2020-08-14 16:47:53 +0000

    Change name for team photo

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def insertteam():
         {
             "team_name": name,
             "score": points,
-            "url": photo
+            "photo": photo
         })
     return redirect(url_for('overview'))
 
