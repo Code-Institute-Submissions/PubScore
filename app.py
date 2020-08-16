0245ad5e55766cdcf43f27d320366d5bf7d2c35d
FrancesVugts <francesvugts@hotmail.com>
2020-08-16 11:49:09 +0000

    Add commands to app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# All the imports needed for this project
 import os
 from flask import Flask, render_template, g, redirect, request, url_for
 from flask_oidc import OpenIDConnect
@@ -6,10 +7,14 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import datetime
 
+# Import my env.py that's ignored by git
 if os.path.exists("env.py"):
     import env
 
+# App instance
 app = Flask(__name__)
+
+# Okta configuration
 SECRET_KEY = os.environ.get("SECRET_KEY")
 app.config["OIDC_CLIENT_SECRETS"] = "client_secrets.json"
 app.config["OIDC_COOKIE_SECURE"] = False
@@ -20,12 +25,15 @@ app.config["OIDC_ID_TOKEN_COOKIE_NAME"] = "oidc_token"
 oidc = OpenIDConnect(app)
 okta_client = UsersClient("https://dev-395248.okta.com",
                           "00SvA-wtwtZrunBeOfMPCmIdo788BQcjWq_81b3dwK")
+
+# MongoDB configuration
 MONGO_URI = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = 'PubScore'
 app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 
 
+# Setup g.user
 @app.before_request
 def before_request():
     if oidc.user_loggedin:
@@ -34,24 +42,30 @@ def before_request():
         g.user = None
 
 
+# Index to welcome the user
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
 
+# Login for admin
 @app.route("/login")
 @oidc.require_login
 def login():
     return redirect(url_for(".dashboard"))
 
 
+# Dashboard after login for some explanation
 @app.route("/dashboard")
 @oidc.require_login
 def dashboard():
     return render_template("dashboard.html")
 
 
+# Overview of all the teams in the competition
+# sorted from highest to lowest score
+# Everybody can view this page
 @app.route("/overview")
 def overview():
     sorted_score = mongo.db.competitors.find().sort('score', -1)
@@ -59,6 +73,10 @@ def overview():
                            competitors=sorted_score)
 
 
+# Overview of all the teams in the competiton
+# From here the admin can add points for all the teams
+# From here the admin can delete teams from the competition
+# Sorted by teamname for easy updating
 @app.route("/updateteams")
 def updateteams():
     sorted_teamname = mongo.db.competitors.find().sort('team_name', 1)
@@ -66,6 +84,9 @@ def updateteams():
                            competitors=sorted_teamname)
 
 
+# Adds the scored points to the original score
+# Adds or updates date of last adjustment by admin
+# After update you stay on the updateteams page to do more updates
 @app.route('/updatescore/<comp_id>/<score>',
            methods=['POST'])
 def updatescore(comp_id, score):
@@ -82,17 +103,22 @@ def updatescore(comp_id, score):
     return redirect(url_for('updateteams'))
 
 
+# Deletes team
+# After update you stay on the updateteams page to do more updates
 @app.route('/deleteteam/<comp_id>')
 def deleteteam(comp_id):
     mongo.db.competitors.remove({'_id': ObjectId(comp_id)})
     return redirect(url_for('updateteams'))
 
 
+# Add a team by using a form
 @app.route("/addteam")
 def addteam():
     return render_template("addteam.html")
 
 
+# Insert team to the database
+# If there is no team photo no-photo.png gets asigned
 @app.route('/insertteam', methods=['POST'])
 def insertteam():
     competitors = mongo.db.competitors
@@ -111,11 +137,13 @@ def insertteam():
     return redirect(url_for('overview'))
 
 
+# Contact page in case of any problems
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
 
+# To run the app
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
